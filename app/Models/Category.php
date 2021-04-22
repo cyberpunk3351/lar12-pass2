0a4f89e7113706f6cf558cf2da4973d999eb7920
Ilya <36233549+cyberpunk3351@users.noreply.github.com>
2021-04-22 15:49:40 +0700

    add test, edit test

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Category extends Model
 {
+
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+    ];
+
+
     public function pass() {
 
         return $this->hasMany(Pass::class,'category_id', 'id');
--- tests/Feature/CrudPasswordTest.php
+++ tests/Feature/CrudPasswordTest.php
@@ -7,13 +7,14 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Pass;
+use App\Models\Category;
 
 class CrudPasswordTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
     /** @test */
-    public function test_auth_users_can_create_pass()
+    public function test_auth_user_can_create_pass()
     {
         $this->actingAs(\App\Models\User::factory()->create());
         $pass = \App\Models\Pass::factory()->make();
@@ -22,20 +23,29 @@ class CrudPasswordTest extends TestCase
     }
 
     /** @test */
-    public function test_unauth_users_cannot_create_pass()
+    public function test_unauth_user_cannot_create_pass()
     {
         $pass = \App\Models\Pass::factory()->make();
         $this->post('/pass',$pass->toArray())->assertRedirect('/login');
     }
 
     /** @test */
-    public function test_admin_user_can_edit_role_for_users()
+    public function test_admin_user_can_edit_roles_for_users()
     {
         $this->actingAs(\App\Models\User::factory()->create(['role_id' => 1]));
         $user = \App\Models\User::factory()->create();
         $user->role_id = "8";
         $this->patch('/role/edit/'.$user->id, $user->toArray());
         $this->assertDatabaseHas('users',['id'=> $user->id , 'role_id' => '8']);
+    }
+
+    /** @test */
+    public function test_admin_user_can_create_categories()
+    {
+        $this->actingAs(\App\Models\User::factory()->create(['role_id' => 1]));
+        $categories = \App\Models\Category::factory()->make();
+        $this->post('/category',$categories->toArray());
+        $this->assertEquals(1,Category::all()->count());
     }
 
 }
